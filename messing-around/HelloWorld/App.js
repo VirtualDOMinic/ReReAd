b44c856e6cdc54bd07c1d6f8cfb1d17bbe44d21f
Dominic Coelho <domwork@live.com>
2019-02-23 13:46:31 +0000

    normalise bg colour

--- messing-around/HelloWorld/App.js
+++ messing-around/HelloWorld/App.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: 'linear-gradient()',
+    backgroundColor: "pink"
   },
   welcome: {
     fontSize: 20,
